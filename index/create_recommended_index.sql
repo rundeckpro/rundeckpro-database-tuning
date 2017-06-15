f41223e4221931c6cb3d4877bbcef254987c5bea
Luis Toledo <luis@variacode.com>
2017-06-15 14:05:30 -0400

    New Index for Activity , Home Page and Delete Executions

--- index/create_recommended_index.sql
+++ index/create_recommended_index.sql
@@ -5,3 +5,9 @@ create index EXECUTION__DATE_COMPLETED__SCHEDULED_EXECUTION_ID 	on execution(dat
 create index WORKFLOW_STEP__ID 									on workflow_step(id);
 create index WORKFLOW_WORKFLOW_STEP__WORKFLOW_COMMANDS_ID 		on workflow_workflow_step(workflow_commands_id);
 
+create index EXECUTION__PROJECT__DATE_STARTED 					on execution(date_started, status);
+create index BASE_REPORT_EXECUTION_ID							on base_report(jc_exec_id);
+create index BASE_REPORT__CLASS__JC_JOB_ID__CTX_PROJECT3 		on base_report(class, ctx_project,jc_job_id,jc_exec_id);
+create index BASE_REPORT__CLASS__JC_JOB_ID__CTX_PROJECT2		on base_report(class, ctx_project,date_completed,date_started);
+create index EXECUTION_PROJECT_NAME_DATE_STARTED 				on execution(id,project,date_completed);
+
